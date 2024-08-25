b9c53ddd39f66af8075b13809ad456e4af7eae54
Ihar Skavysh <igogoblin@gmail.com>
2024-08-25 21:22:00 +0300

    feat: add last 2 task solutions in numbers-tasks

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -209,8 +209,17 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  // throw new Error('Not implemented');
+  if (n < 2) {
+    return false;
+  }
+  for (let i = 2; i < n; i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
@@ -228,8 +237,13 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  // throw new Error('Not implemented');
+  // return typeof value === 'number' ? value : def;
+  if (typeof value === 'number' || value instanceof Number) {
+    return Number(value);
+  }
+  return def;
 }
 
 module.exports = {
