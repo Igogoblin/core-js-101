8c26fd19c93378087e36d2b5765ee5f60b9e720e
Ihar Skavysh <igogoblin@gmail.com>
2024-08-31 19:53:46 +0300

    feat: add getidentitymatrix solution in arrays-tasks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -481,11 +481,13 @@ function sortCitiesArray(arr) {
  *           [0,0,0,1,0],
  *           [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  // throw new Error('Not implemented');
   // return Array.from({ length: n }, (_, i) => {
   //   Array.from({ length: n }, (_, j) => (i === j ? 1 : 0));
   // });
+  // eslint-disable-next-line no-shadow, max-len
+  return Array.from({ length: n }, (_, index) => Array.from({ length: n }, (_, i) => (i === index ? 1 : 0)));
 }
 
 /**
